65054c0ee069b553187bbf809cb07f3ced453c0c
kubo314 <147988437+kubo314@users.noreply.github.com>
2025-01-04 21:56:50 +0900

    Update new4.js
    css用追加

--- new4.js
+++ new4.js
@@ -1,50 +1,51 @@
-        function wiki(url) {
-            (async function () {
-                const parser = new DOMParser();
+async function wiki(url) {
+    document.querySelector("button").classList.add("onclic");
+    const parser = new DOMParser();
 
-                const res = await fetch(`https://ja.wikipedia.org/w/api.php?format=xml&action=query&prop=revisions&titles=${url}&rvprop=content&rvparse&origin=*`);
-                const data = await res.text();
-                const txt = await parser.parseFromString(data, "text/xml");
-                const html = txt.querySelector("rev");
-                document.querySelector("main").innerHTML = html.textContent;
+    const res = await fetch(`https://ja.wikipedia.org/w/api.php?format=xml&action=query&prop=revisions&titles=${url}&rvprop=content&rvparse&origin=*`);
+    const data = await res.text();
+    const txt = await parser.parseFromString(data, "text/xml");
+    const html = txt.querySelector("rev");
+    document.querySelector("main").innerHTML = html.textContent;
 
-                const images = document.querySelectorAll("img, video, audio, source");
+    const images = document.querySelectorAll("img, video, audio, source");
 
-                images.forEach(img => {
-                    const src = img.getAttribute("src");
-                    const srcset = img.getAttribute("srcset");
+    images.forEach(img => {
+        const src = img.getAttribute("src");
+        const srcset = img.getAttribute("srcset");
 
-                    if (src) {
-                        img.setAttribute("src", "https:" + src);
-                    }
-                    if (srcset) {
-                        img.setAttribute("srcset", srcset.replaceAll("//", "https://"));
-                    }
-                });
-
-                const a_elements = document.querySelectorAll("a");
-
-                a_elements.forEach(a_element => {
-                    const href = a_element.getAttribute("href");
-                    
-                    if (href) {
-                        try {
-                            let changed_href = href.replace("/wiki/", window.location.href.replace(location.search , '') + "?url=")
-                            a_element.setAttribute("href", changed_href);
-                        } catch (e) {
-                        }
-                    }
-                });
-            })();
+        if (src) {
+            img.setAttribute("src", "https:" + src);
         }
-        window.addEventListener("load", function () {
-            const url = new URL(window.location.href);
-            const params = url.searchParams;
-            document.querySelector("input").value = params.get("url")
-            if (params.get("url") !== null) {
-                wiki(params.get("url"));
+        if (srcset) {
+            img.removeAttribute("srcset");
+        }
+    });
+
+    const a_elements = document.querySelectorAll("a");
+
+    a_elements.forEach(a_element => {
+        const href = a_element.getAttribute("href");
+
+        if (href) {
+            try {
+                let changed_href = href.replace("/wiki/", window.location.href.replace(location.search, '') + "?url=")
+                a_element.setAttribute("href", changed_href);
+            } catch (e) {
             }
-        });
-        document.querySelector("button").addEventListener("click", function () {
-            wiki(document.querySelector("input").value);
-        });
+        }
+    });
+
+    document.querySelector("button").classList.remove("onclic");
+};
+window.addEventListener("load", function () {
+    const url = new URL(window.location.href);
+    const params = url.searchParams;
+    document.querySelector("input").value = params.get("url")
+    if (params.get("url") !== null) {
+        wiki(params.get("url"));
+    }
+});
+document.querySelector("button").addEventListener("click", async function () {
+    await wiki(document.querySelector("input").value);
+});
